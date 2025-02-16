51e5b41421db8466aba7a91c32ac81833a35b912
kikitongky <tongkytongky07@gmail.com>
2025-02-16 13:40:46 +0700

    initialization commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="my-3 mx-2 justify-center items-start">
+      <h1 className="text-3xl font-bold">One Sentence</h1>
+    </div>
+  );
+}
